649756b864f1b8e75b78deb50017abe061b62284
taranjob <979210641@qq.com>
2016-08-22 19:34:02 +0800

    原生js判断滚动到底部
    去掉jquery依赖

--- js/com.js
+++ js/com.js
@@ -63,7 +63,7 @@ function TouchPageEvent() {
             ondrag = true;
             direction='down';
         }
-        if (ev.direction === 'up' && ($(document).scrollTop() >= $(document).height() - $(window).height() ) ){
+        if (ev.direction === 'up' && (getScrollTop() + getWindowHeight() == getScrollHeight() ) ){
             ondrag = true;
             direction='up';
         }
@@ -128,3 +128,45 @@ function TouchPageEvent() {
         }
     })
 }
+
+//滚动条在Y轴上的滚动距离
+
+function getScrollTop(){
+    var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
+    if(document.body){
+        bodyScrollTop = document.body.scrollTop;
+    }
+    if(document.documentElement){
+        documentScrollTop = document.documentElement.scrollTop;
+    }
+    scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+    return scrollTop;
+}
+
+//文档的总高度
+
+function getScrollHeight(){
+    var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+    if(document.body){
+        bodyScrollHeight = document.body.scrollHeight;
+    }
+    if(document.documentElement){
+        documentScrollHeight = document.documentElement.scrollHeight;
+    }
+    scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+    return scrollHeight;
+}
+
+//浏览器视口的高度
+
+function getWindowHeight(){
+    var windowHeight = 0;
+    if(document.compatMode == "CSS1Compat"){
+        windowHeight = document.documentElement.clientHeight;
+    }else{
+        windowHeight = document.body.clientHeight;
+    }
+    return windowHeight;
+}
+
+
